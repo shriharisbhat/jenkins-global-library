93e2c757a82b48329f1f2962367cc0cc423be0a6
sriharisbhat <srihari.bhat@robosoftin.com>
2024-07-22 13:37:16 +0530

    chore: Create an instance of PodTemplates

--- vars/multiToolPod.groovy
+++ vars/multiToolPod.groovy
@@ -4,6 +4,8 @@ def call(Map args = [:], Closure body) {
   def label = args.label ?: "multi-tool-${UUID.randomUUID().toString()}"
   def cloudName = args.cloud ?: 'kubernetes'
 
+  def podTemplates = new PodTemplates() // Create an instance of PodTemplates
+
   podTemplates.dockerTemplate(cloudName: cloudName) {
     podTemplates.awsCliTemplate {
       node(POD_LABEL) {
