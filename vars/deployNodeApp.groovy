21290d3d14540033735278d94cb4b7c62d4e9ed5
sriharisbhat <srihari.bhat@robosoftin.com>
2024-07-23 21:25:23 +0530

    chore: add node

--- vars/deployNodeApp.groovy
+++ vars/deployNodeApp.groovy
@@ -2,7 +2,9 @@ def call(Map config) {
     stage("Deploying node to ${config.namespace}") {
         lock("deploy-${config.namespace}-${config.applicationName}") {
             echo "Locked build pipeline for deployment of ${config.applicationName}"
-            sh "echo deploying ${config.applicationName} to ${config.namespace}"
+            node(POD_LABE) {
+                sh "echo deploying ${config.applicationName} to ${config.namespace}"
+            }
 
         }
     }
