3d6bbf64cbbddb451d123532249f6975c4719973
sriharisbhat <srihari.bhat@robosoftin.com>
2024-07-23 21:25:59 +0530

    chore: correct the name

--- vars/deployNodeApp.groovy
+++ vars/deployNodeApp.groovy
@@ -2,7 +2,7 @@ def call(Map config) {
     stage("Deploying node to ${config.namespace}") {
         lock("deploy-${config.namespace}-${config.applicationName}") {
             echo "Locked build pipeline for deployment of ${config.applicationName}"
-            node(POD_LABE) {
+            node(POD_LABEL) {
                 sh "echo deploying ${config.applicationName} to ${config.namespace}"
             }
 
