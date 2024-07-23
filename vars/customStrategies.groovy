e5c3e1140c703750e3dc0b8033d6fa9cdb988497
sriharisbhat <srihari.bhat@robosoftin.com>
2024-07-23 20:11:39 +0530

    chore: make it more flexible and customization

---
+++ vars/customStrategies.groovy
@@ -0,0 +1,46 @@
+def in_parallel(List tasks) {
+    return { config ->
+        def tasksMap = [:]
+        tasks.each { task ->
+            tasksMap["task-${tasks.indexOf(task)}"] = { task(config) }
+        }
+        parallel(tasksMap)
+    }
+}
+
+def sequence(List tasks) {
+    return { config ->
+        tasks.each { task ->
+            task(config)
+        }
+    }
+}
+
+def autoApproval(String namespace) {
+    return { config ->
+        stage("Auto Deploy ${namespace}") {
+            echo "Auto deploying to ${namespace}"
+            // Trigger the provided doDeploy function
+            if (config.doDeploy) {
+                config.doDeploy(config + [namespace: namespace])
+            } else {
+                error "doDeploy function not provided."
+            }
+        }
+    }
+}
+
+def manualApproval(String namespace) {
+    return { config ->
+        stage("Manual Deploy ${namespace}") {
+            input message: "Approve deployment to ${namespace}?", ok: "Approve"
+            echo "Deploying to ${namespace}"
+            // Trigger the provided doDeploy function
+            if (config.doDeploy) {
+                config.doDeploy(config + [namespace: namespace])
+            } else {
+                error "doDeploy function not provided."
+            }
+        }
+    }
+}
--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -1,31 +1,50 @@
 def call(Map config) {
-    def strategy = config.strategy ?: { [] }
+    def strategy = config.strategy ?: { throw new MissingMethodException("Required strategy like parallel or sequence not defined", this.class, []) }
     
+    // Execute each task defined in the strategy
     strategy().each { task ->
         task(config)
     }
-}
-
-def doDeploy(Map config, String namespace) {
-    stage("Deploying to ${namespace}") {
-        lock("deploy-${namespace}-${config.applicationName}") {
-            echo "Locked build pipeline for deployment of ${config.applicationName}"
-
-        }
+    
+    // Execute the deployment logic
+    def doDeploy = config.doDeploy
+    
+    if (doDeploy) {
+        doDeploy(config)
+    } else {
+        throw new MissingMethodException("custom doDeploy method not defined", this.class, [])
     }
 }
 
-def getEnvironmentConfig() {
-    // Implementation to get environment config
-}
 
-def getClusterKubeConfigPath(cluster) {
-    // Implementation to get cluster kubeconfig path
-}
+// def call(Map config) {
+//     def strategy = config.strategy ?: { [] }
+    
+//     strategy().each { task ->
+//         task(config)
+//     }
+// }
 
-def checkoutInfraRepo() {
-    // Implementation to checkout infra repo
-}
+// def doDeploy(Map config, String namespace) {
+//     stage("Deploying to ${namespace}") {
+//         lock("deploy-${namespace}-${config.applicationName}") {
+//             echo "Locked build pipeline for deployment of ${config.applicationName}"
+
+//         }
+//     }
+// }
+
+// def getEnvironmentConfig() {
+//     // Implementation to get environment config
+// }
+
+// def getClusterKubeConfigPath(cluster) {
+//     // Implementation to get cluster kubeconfig path
+// }
+
+// def checkoutInfraRepo() {
+//     // Implementation to checkout infra repo
+// }
 
 def performDeploy(String clusterKubeConfig, envConfig) {
     // Implementation to perform deploy
