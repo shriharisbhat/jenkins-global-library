c0e70c2057d101e484df22b5f7a6eaf4e9ff5653
sriharisbhat <srihari.bhat@robosoftin.com>
2024-07-23 20:51:19 +0530

    chore: revert to call

--- vars/deployWebApp.groovy
+++ vars/deployWebApp.groovy
@@ -1,4 +1,4 @@
-def doDeploy(Map config) {
+def call(Map config) {
     stage("Deploying to ${config.namespace}") {
         lock("deploy-${config.namespace}-${config.applicationName}") {
             echo "Locked build pipeline for deployment of ${config.applicationName}"
