5616f59d3cab949b9edfa1303d32ecb7f041b498
sriharisbhat <srihari.bhat@robosoftin.com>
2024-07-24 12:54:44 +0530

    chore: add multiToolPod deployWebApp job

--- vars/deployWebApp.groovy
+++ vars/deployWebApp.groovy
@@ -1,8 +1,32 @@
 def call(Map config) {
-    stage("Deploying to ${config.namespace}") {
-        lock("deploy-${config.namespace}-${config.applicationName}") {
+      stage("Deploying to ${config.namespace}") {
+         lock("deploy-${config.namespace}-${config.applicationName}") {
             echo "Locked build pipeline for deployment of ${config.applicationName}"
+            multiToolPod(cloud: 'kubernetes', namespace: 'jenkins') {
+               container('docker') {
+                  sh 'docker version'
+                  sh 'docker pull nginx:latest'
+               }
+               container('aws-cli') {
+                  // withCredentials([file(credentialsId: 'aws-credentials', variable: 'AWS_SHARED_CREDENTIALS_FILE')]) {
+                  sh 'aws --version'
+                  // sh 'aws s3 ls'
+                  // }
+               }
+               // container('kubectl') {
+               //     // withCredentials([file(credentialsId: 'kubeconfig', variable: 'KUBECONFIG')]) {
+               //          sh 'echo $HOSTNAME'
+               //          sh 'env | sort'
+               //          sh 'which kubectl || echo "kubectl not found"'
+               //          sh '''
+               //             set -x
+               //             kubectl version --client
+               //             env | sort
+               //          '''
 
-        }
-    }
-}
+               //     // }
+               // }
+
+            }
+         }
+      }
