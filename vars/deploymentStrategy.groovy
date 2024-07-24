a049ab9b0c51405a92bf53086b5a00256b8eb992
sriharisbhat <srihari.bhat@robosoftin.com>
2024-07-24 13:42:35 +0530

    chore: test with collectEntries

--- vars/deploymentStrategy.groovy
+++ vars/deploymentStrategy.groovy
@@ -1,10 +1,18 @@
+// def in_parallel(List tasks) {
+//     return { config ->
+//         def tasksMap = [:]
+//         tasks.each { task ->
+//             tasksMap["task-${tasks.indexOf(task)}"] = { task(config) }
+//         }
+//         parallel(tasksMap)
+//     }
+// }
+
 def in_parallel(List tasks) {
     return { config ->
-        def tasksMap = [:]
-        tasks.each { task ->
-            tasksMap["task-${tasks.indexOf(task)}"] = { task(config) }
-        }
-        parallel(tasksMap)
+        parallel(tasks.collectEntries { task ->
+            ["task-${tasks.indexOf(task)}": { task(config) }]
+        })
     }
 }
 
