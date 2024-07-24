1b1b181886bc432eb5d0edca4cc9e1a4338c43b5
sriharisbhat <srihari.bhat@robosoftin.com>
2024-07-24 13:56:07 +0530

    chore: push refactors

--- vars/deploymentStrategy.groovy
+++ vars/deploymentStrategy.groovy
@@ -1,13 +1,3 @@
-// def in_parallel(List tasks) {
-//     return { config ->
-//         def tasksMap = [:]
-//         tasks.each { task ->
-//             tasksMap["task-${tasks.indexOf(task)}"] = { task(config) }
-//         }
-//         parallel(tasksMap)
-//     }
-// }
-
 def in_parallel(List tasks) {
     return { config ->
         parallel(tasks.collectEntries { task ->
