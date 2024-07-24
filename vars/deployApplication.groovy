cc2a63771f5db298e5f1cfd1f27a234c1217263f
sriharisbhat <srihari.bhat@robosoftin.com>
2024-07-24 19:22:00 +0530

    chore: remove check

--- vars/deployApplication.groovy
+++ vars/deployApplication.groovy
@@ -2,10 +2,10 @@ def call(Map config) {
     def strategyClosure = config.strategy ?: { throw new MissingMethodException("Required strategy like parallel or sequence not defined", this.class, []) }
     
     // Retrieve deployment logic
-    def doDeploy = config.doDeploy
-    if (!doDeploy) {
-        throw new MissingMethodException("doDeploy not defined", this.class, [])
-    }
+    // def doDeploy = config.doDeploy
+    // if (!doDeploy) {
+    //     throw new MissingMethodException("doDeploy not defined", this.class, [])
+    // }
     
     // Execute each task like in_parallel() or sequence() defined in the strategy, checkout deploymentStrategy.groovy
     strategyClosure().each { task ->
