f09d86a5f8328ee6149d00815e3c0c84ef1a4fc6
sriharisbhat <srihari.bhat@robosoftin.com>
2024-07-24 13:25:23 +0530

    chore: improve manual approval process

--- vars/approvalStrategy.groovy
+++ vars/approvalStrategy.groovy
@@ -13,16 +13,33 @@ def autoApproval(String namespace) {
     }
 }
 
-def manualApproval(String namespace) {
+def manualApproval(String namespace, int timeoutMinutes = 60) {
     return { config ->
-        stage("Manual Approval Deployment") {
-            input message: "Approve deployment to ${namespace}?", ok: "Approve"
-            echo "Deploying to ${namespace}"
-            // Trigger the provided doDeploy function
-            if (config.doDeploy) {
-                config.doDeploy(config + [namespace: namespace])
+        stage("Manual Deploy ${namespace}") {
+            def approved = false
+            try {
+                timeout(time: timeoutMinutes, unit: 'MINUTES') {
+                    approved = input(
+                        message: "Approve deployment to ${namespace}?",
+                        ok: "Approve",
+                        submitterParameter: 'APPROVER'
+                    )
+                }
+            } catch (org.jenkinsci.plugins.workflow.steps.FlowInterruptedException e) {
+                // This exception is thrown when the timeout is exceeded or the input is aborted
+                echo "Deployment to ${namespace} was not approved within ${timeoutMinutes} minutes or was aborted."
+                return // Exit the closure without deploying
+            }
+
+            if (approved) {
+                echo "Deployment to ${namespace} approved. Proceeding with deployment."
+                if (config.doDeploy) {
+                    "${config.doDeploy}"(config + [namespace: namespace])
+                } else {
+                    error "doDeploy function not provided."
+                }
             } else {
-                error "doDeploy function not provided."
+                echo "Deployment to ${namespace} was not approved."
             }
         }
     }
