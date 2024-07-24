6aa40d1bd4a4d90fceefc3f2fa8a61b34e2c0551
sriharisbhat <srihari.bhat@robosoftin.com>
2024-07-24 19:55:32 +0530

    print task

--- vars/deployApplication.groovy
+++ vars/deployApplication.groovy
@@ -9,6 +9,7 @@ def call(Map config) {
     
     // Execute each task like in_parallel() or sequence() defined in the strategy, checkout deploymentStrategy.groovy
     strategyClosure().each { task ->
+        println "tasks $task"
         task(config)
     }
 }
